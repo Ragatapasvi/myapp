6f188723382128e63909613dfaef13d03fb73097
Ragatapasvi <ragatapasvip@gmail.com>
2024-11-29 18:33:09 +0530

    added

---
+++ src/Admin.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+
+export default function Admin() {
+    const [name,setName] = useState()
+    const [price,setPrice] = useState()
+    const [desc,setDesc] = useState()
+    const [url,setURL] = useState()
+    const api = "http://localhost:8080/"
+    const addProduct = async() =>{
+        await axios.post(api,{name:name,price:price,desc:desc,url:url})
+    }
+  return (
+    <div>Admin
+        <p><input type="text" onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'></input></p>
+        <p><input type="text" onChange={(e)=>setPrice(e.target.value)} placeholder='Enter Price'></input></p>
+        <p><input type="text" onChange={(e)=>setDesc(e.target.value)} placeholder='Enter Description'></input></p>
+        <p><input type="text" onChange={(e)=>setURL(e.target.value)} placeholder='Enter URL'></input></p>
+        <button onClick={addProduct}>Save</button>
+     </div>
+
+    
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import Header from "./Header";
+// import Content from "./Content";
+// import Footer from "./Footer";
+//import Login from './Login';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Cart from './Cart';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<>
+      <App/>
+      <Cart/>
+      
+      {/* <Header/>
+      <Content/>
+      <Footer/> */}
+      </>
+       /*{/* //<h1>Hello World!!</h1>
+       //<p>This is a paragraph</p>
+       //<Login/>
+       //</> */
+
+
+);
